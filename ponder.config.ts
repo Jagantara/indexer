fd70f9d6a890c2f58db4431e9650aff3fa7dfb0e
Daviskelvin824 <daviskelvin824@gmail.com>
2025-07-05 22:08:43 +0700

    first commit

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,20 +1,32 @@
 import { createConfig } from "ponder";
-
-import { ExampleContractAbi } from "./abis/ExampleContractAbi";
+import { http } from "viem";
+import { ERC20_ABI, JAGA_STAKE_ABI } from "./abis/StakeAbi";
 
 export default createConfig({
   chains: {
-    mainnet: {
-      id: 1,
-      rpc: process.env.PONDER_RPC_URL_1!,
+    monadTestnet: {
+      id: 10143,
+      rpc: http("https://testnet-rpc.monad.xyz"),
     },
   },
   contracts: {
-    ExampleContract: {
-      chain: "mainnet",
-      abi: ExampleContractAbi,
-      address: "0x0000000000000000000000000000000000000000",
-      startBlock: 1234567,
+    JagaStake: {
+      chain: "monadTestnet",
+      address: "0xB6Dc49E506c5695A3004f3d3946768293a5E7cA6",
+      abi: JAGA_STAKE_ABI,
+      startBlock: 24804508,
+    },
+    JagaToken: {
+      chain: "monadTestnet",
+      address: "0x181bb7749c9bF583cB62832881f98859A734c573",
+      abi: ERC20_ABI,
+      startBlock: 24804508,
+    },
+    USDC: {
+      chain: "monadTestnet",
+      address: "0xeBf52E1C86d75b57f078dD4cD26727cD5cD59ec9",
+      abi: ERC20_ABI,
+      startBlock: 24804508,
     },
   },
 });
